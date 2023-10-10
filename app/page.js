4ad63f6e745ac6a6767c6bb7b68a2f9b1119882c
MubashirLabar <61319180+MubashirLabar@users.noreply.github.com>
2023-10-10 22:19:13 +0500

    landing page

---
+++ app/page.js
@@ -0,0 +1,72 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="text-gray-900">
+      <div
+        class="h-screen pb-14 bg-right bg-cover"
+        style={{ backgroundImage: `url(/bg.svg)` }}
+      >
+        {/* Navbar */}
+        <div className="w-full container mx-auto p-3">
+          <div className="w-full flex items-center justify-between">
+            <Link href={"/"}>
+              <img
+                src="/logo.svg"
+                className="w-[150px] md:w-[180px] absolute"
+              />
+            </Link>
+          </div>
+        </div>
+
+        {/* Main */}
+        <div className="container px-6 mx-auto mt-36 sm:mt-40 xl:mt-0 flex flex-wrap flex-col md:flex-row items-center">
+          {/* Left Side */}
+          <div className="flex flex-col w-full xl:w-2/5  justify-center lg:items-start overflow-y-hidden">
+            <h1 className="my-4 text-3xl md:text-8xl uppercase text-primary-700 font-bold text-center md:text-left">
+              Workout With Me
+            </h1>
+            <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left text-gray-700">
+              Interval Training Timer perfect for HIIT Circuit and Tabata
+              Workouts
+            </p>
+
+            <p className="text-secondary-700 font-bold pb-8 lg:pb-6 text-center md:text-left fade-in">
+              Download our app:
+            </p>
+            <div className="flex w-full justify-center md:justify-start pb-24 lg:pb-0 fade-in">
+              <img
+                src="/app-store.svg"
+                className="h-12 pr-4 bounce-top-icons"
+              />
+              <img src="/play-store.svg" className="h-12 bounce-top-icons" />
+            </div>
+          </div>
+
+          {/* Right Side */}
+          <div class="w-full xl:w-3/5 overflow-hidden">
+            <img
+              class="h-full lg:h-[900px] w-full mx-auto lg:mr-0"
+              src="/vector.svg"
+            />
+          </div>
+
+          {/* Footer */}
+          <div class="w-full flex items-center pb-8 text-sm text-center md:text-left fade-in">
+            <div class="flex-1 text-gray-500 no-underline hover:no-underline">
+              &copy; 2023, Workout Timer
+            </div>
+            <div className="flex items-center gap-x-8">
+              <Link href={"/privacy"} className="text-gray-500">
+                Privacy
+              </Link>
+              {/* <Link href={"/"} className="text-gray-500">
+                Contact Us
+              </Link> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ app/privacy/page.js
@@ -0,0 +1,72 @@
+import Link from "next/link";
+
+function Privacy() {
+  return (
+    <div class="w-screen">
+      {/* Navbar */}
+      <div className="w-full container mx-auto p-3">
+        <div className="w-full flex items-center justify-between">
+          <Link href={"/"}>
+            <img src="/logo.svg" className="w-[150px] md:w-[180px] absolute" />
+          </Link>
+        </div>
+      </div>
+      <div className="w-full max-w-[900px] mx-auto p-3 mt-32 md:mt-36 pb-12 md:pb-16">
+        <div className="text-4xl md:text-5xl text-gray-900 font-semibold mb-8">
+          Privacy
+        </div>
+        <div className="w-full flex flex-col gap-7 text-sm text-gray-900">
+          <div>{`This Privacy Policy explains how Workout Timer ("we," "our," or "us") handles user data when you use our mobile application ("App"). By using our App, you agree to the terms outlined in this Privacy Policy.`}</div>
+
+          <div className="text-base font-semibold">
+            Information We Do Not Collect
+          </div>
+          <div>{`We want to assure you that we do not collect any personal information or utilize third-party services for data collection when you use our App. We respect your privacy and believe in maintaining transparency.`}</div>
+
+          <div className="text-base font-semibold">Log Data</div>
+          <div>{`When you use our App, we may collect limited data for error tracking and performance improvement purposes. This information, known as Log Data, may include generic details such as device information, operating system version, and app usage statistics. However, we do not collect any personally identifiable information through this process.`}</div>
+
+          <div className="text-base font-semibold">Cookies</div>
+          <div>{`Our App does not use cookies or similar tracking technologies to collect user data.`}</div>
+
+          <div className="text-base font-semibold">Service Providers</div>
+          <div>{`We do not engage any third-party companies or individuals to collect, process, or manage user data on our behalf.`}</div>
+
+          <div className="text-base font-semibold">Security</div>
+          <div>{`While we do not collect personal information, we are committed to safeguarding your data and maintaining the security of our App to the best of our ability.`}</div>
+
+          <div className="text-base font-semibold">Links to Other Sites</div>
+          <div>{`Our App may contain links to external websites or services. Please be aware that these sites are not operated by us, and we encourage you to review their respective privacy policies. We have no control over and assume no responsibility for the content, privacy practices, or policies of third-party websites.`}</div>
+
+          <div className="text-base font-semibold">{`Children's Privacy`}</div>
+          <div>{`Our App is not designed for users under the age of 13, and we do not knowingly collect any personal information from children under 13. If you believe that a child has provided us with personal information, please contact us, and we will take appropriate action.`}</div>
+
+          <div className="text-base font-semibold">
+            Changes to This Privacy Policy
+          </div>
+          <div>{`We may update our Privacy Policy in the future. It is your responsibility to review this page for any changes. We will notify you of any updates by posting the revised Privacy Policy on this page.`}</div>
+
+          <div className="text-base font-semibold">Contact Us</div>
+          <div>
+            If you have any questions or concerns regarding our Privacy Policy,
+            please do not hesitate to contact us at{" "}
+            <a
+              href="https://zeezsoft.com/"
+              target="_blank"
+              className="text-blue-700"
+            >
+              zeezsoft
+            </a>
+          </div>
+          <div>
+            Thank you for using the Workout Timer App. We prioritize your
+            privacy and are committed to providing a safe and secure user
+            experience.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Privacy;
